d63e4063b5fff6234c15164dc23c004633690563
Pharniyi Hesther <faniyitimi@gmail.com>
2025-03-28 02:05:07 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../src/components/Navbar/Navbar'
-
+import Hero from '../src/components/Hero/Hero'
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
